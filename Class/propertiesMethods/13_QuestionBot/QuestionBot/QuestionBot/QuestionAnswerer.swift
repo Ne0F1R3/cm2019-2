2d7318256f4faa6a6295a0190b29e969d83021c9
Erick I Bárcenas M <42219451+erickbarcenas@users.noreply.github.com>
2019-02-27 11:29:54 -0600

    try a question!
    Change Bot icon

---
+++ Class/propertiesMethods/13_QuestionBot/QuestionBot/QuestionBot/QuestionAnswerer.swift
@@ -0,0 +1,14 @@
+struct MyQuestionAnswerer {
+    func responseTo(question: String) -> String {
+        
+        let lowerQuestion = question.lowercased()
+        
+        if lowerQuestion.hasPrefix("where") {
+            return "To the North!"
+        } else if lowerQuestion == "where are the cookies?" {
+            return "In the cookie jar!"
+        } else {
+            return "That really depends"
+        }
+    }
+}
