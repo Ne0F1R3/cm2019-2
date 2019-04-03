e801cf5174e57fc250e35750940c6b6f77d6a2fe
Erick I Bárcenas M <42219451+erickbarcenas@users.noreply.github.com>
2019-04-03 11:31:36 -0600

    Create Your Storyboard Scenes With Simple Segues
    “App Development with Swift”

---
+++ Class/Login/Login/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Macbook on 4/3/19.
+//  Copyright © 2019 Sauzun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var UserName: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UserName.textColor = UIColor.lightGray
+        Password.textColor = UIColor.lightGray
+    }
+    
+
+    @IBAction func LogInAction(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+}
+
