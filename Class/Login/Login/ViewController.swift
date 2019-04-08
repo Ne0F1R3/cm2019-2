d6d118b7737b8b5a95d5a33942b2a115241ae2af
Erick I Bárcenas M <42219451+erickbarcenas@users.noreply.github.com>
2019-04-08 11:35:09 -0500

    Segue Login - Safe Area

--- Class/Login/Login/ViewController.swift
+++ Class/Login/Login/ViewController.swift
@@ -13,16 +13,43 @@ class ViewController: UIViewController {
     @IBOutlet weak var UserName: UITextField!
     @IBOutlet weak var Password: UITextField!
     
+    @IBOutlet weak var forgotUsr: UIButton!
+    @IBOutlet weak var forgotPwd: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         UserName.textColor = UIColor.lightGray
         Password.textColor = UIColor.lightGray
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //segue.destination.navigationItem.title = UserName.text
+        guard let sender = sender as? UIButton else {return}
+        if sender == forgotUsr{
+            segue.destination.navigationItem.title = "forgot username"
+        }else if sender == forgotPwd{
+            segue.destination.navigationItem.title = "forgot password"
+        }else {
+            segue.destination.navigationItem.title = UserName.text
+        }
+        
+        
+        
+    }
 
     @IBAction func LogInAction(_ sender: Any) {
     }
     
+    
+    @IBAction func forgotUsrBtn(_ sender: UIButton) {
+        performSegue(withIdentifier: "forgotUsrPwd", sender: forgotUsr)
+    }
+    
+    
+    @IBAction func forgotPwd(_ sender: UIButton) {
+        performSegue(withIdentifier: "forgotUsrPwd", sender: forgotPwd)
+    }
     
     
     
