d52416cfb39104fff35efbafb74cd590cd40ac0c
Erick I Bárcenas M <42219451+erickbarcenas@users.noreply.github.com>
2019-04-10 10:09:00 -0500

    UI

---
+++ Tareas/Tarea 4/Calculator/Calculator/Calculator_x/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Macbook on 4/10/19.
+//  Copyright © 2019 Barcenas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+}
+
